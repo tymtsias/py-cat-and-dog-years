2bc7158076d21d850b7c58da424f0ea38949339b
tym <premo1313@gmail.com>
2026-09-06 19:01:18 +0300

    requested changes

--- app/main.py
+++ app/main.py
@@ -3,21 +3,24 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
     if cat_age < 0 and dog_age < 0:
         raise ValueError("Cat and dog ages cannot be negative.")
 
-    if not isinstance(cat_age, int) and not isinstance(dog_age, int):
+    if not isinstance(cat_age, int) or not isinstance(dog_age, int):
         raise TypeError("The 'cat_age' and 'dog_age' must be an integer.")
 
     if cat_age < 15:
         cat_result = 0
-    elif cat_age == 15 or cat_age <= 24:
+    elif cat_age == 15 or cat_age <= 23:
         cat_result = 1
-    elif cat_age > 24:
+    elif cat_age >= 24:
         cat_result = 2 + (cat_age - 24) // 4
 
     if dog_age < 15:
         dog_result = 0
-    elif dog_age == 15 or dog_age <= 24:
+    elif dog_age == 15 or dog_age <= 23:
         dog_result = 1
-    elif dog_age > 24:
+    elif dog_age >= 24:
         dog_result = 2 + (dog_age - 24) // 5
 
     return [cat_result, dog_result]
+
+
+print(get_human_age(23, 24))
--- app/test_main.py
+++ app/test_main.py
@@ -1,29 +1,25 @@
+import pytest
+
 from app.main import get_human_age
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (16, 16, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+    ],
+)
+def test_ages(cat_age, dog_age, expected):
+    assert get_human_age(cat_age, dog_age) == expected
 
 
 def test_should_return_array_of_two_elements() -> None:
     assert len(get_human_age(15, 15)) == 2
-
-
-def test_should_zero_values_if_zero() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_should_return_zero_values_if_less_than_15() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_should_return_values_if_15() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_should_return_values_if_more_than_15() -> None:
-    assert get_human_age(16, 16) == [1, 1]
-
-
-def test_should_return_values_if_23() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_should_return_values_with_big_age_more() -> None:
-    assert get_human_age(100, 100) == [21, 17]
